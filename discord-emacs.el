7468c48a4082eb9954d96b820a93e44cd74b7e93
ben simms <ben@bensimms.moe>
2017-11-28 19:32:37 +0000

    Add: We're a good

--- discord-emacs.el
+++ discord-emacs.el
@@ -1,22 +1,23 @@
 (require 'json)
 (require 'bindat)
-(require 'timer)
 
-(defvar +handshake+ 0)
-(defvar +frame+ 1)
-(defvar +close+ 2)
-(defvar +ping+ 3)
-(defvar +pong+ 4)
+(defvar discord-ipc-+handshake+ 0)
+(defvar discord-ipc-+frame+ 1)
+(defvar discord-ipc-+close+ 2)
+(defvar discord-ipc-+ping+ 3)
+(defvar discord-ipc-+pong+ 4)
 
-(setq discord-ipc-spec
+(defvar discord-ipc-spec
   '((opcode u32r)
     (length u32r)
     (data str (length))))
 
-(setq discord-rich-presence-version 1)
+(defvar discord-ipc-rich-presence-version 1)
 
-(setq discord-ipc-client-id nil)
-(setq discord-ipc-client-timer nil)
+(defvar discord-ipc-process nil)
+(defvar discord-ipc-client-id nil)
+(defvar discord-ipc-current-buffer nil)
+(defvar discord-ipc-started nil)
 
 (defun get-ipc-url ()
   (format "/run/user/%i/discord-ipc-0" (user-uid)))
@@ -24,7 +25,6 @@
 (defun make-ipc-connection ()
   "Make a ipc socket connection"
   (make-network-process :name "discord-emacs"
-                        :buffer "*discord-rich-presence*"
                         :remote (get-ipc-url)
                         :nowait nil))
 
@@ -32,12 +32,13 @@
   (let* ((decoded (unpack-data data))
          (evt (cdr (assq 'evt (cdr decoded)))))
     (cond ((string= evt "READY")
-           (setq ipc-ready t)
-           (setq current-connection process)
-           (send-json process +frame+ (gather-data))
-           (run-at-time "1 min" nil (lambda () (delete-process process)))))))
-          ;;((string= evt "ERROR")
-          ;; (print decoded)))))
+           (setq discord-ipc-current-connection process)
+           (send-json process discord-ipc-+frame+ (gather-data))))))
+
+(defun handle-ipc-evt (process evt)
+  (when (string= evt "connection broken by remote peer\n")
+    (delete-process process)
+    (setq discord-ipc-process nil)))
 
 (defun pack-data (opcode data)
   (let ((encoded-json (json-encode data)))
@@ -51,7 +52,7 @@
           (json-read-from-string (bindat-get-field unpacked 'data)))))
 
 (defun ipc-handshake (client-id)
-  `((v .  ,discord-rich-presence-version)
+  `((v .  ,discord-ipc-rich-presence-version)
     (client_id . ,client-id)))
 
 (defun rich-presence (&rest fields)
@@ -61,12 +62,17 @@
     (nonce . ,(number-to-string (random)))))
 
 (defun send-json (process opcode data)
-  (process-send-string process (pack-data opcode data)))
+  ;; (message "Sending discord-ipc op: %d data: %s" opcode data)
+  (if process
+      (process-send-string process (pack-data opcode data))
+    (ipc-connect discord-ipc-client-id)))
 
-(defun ipc-update ()
+(defun ipc-connect (client-id)
   (let ((process (make-ipc-connection)))
     (set-process-filter process 'handle-ipc-connection)
-    (send-json process +handshake+ (ipc-handshake discord-ipc-client-id))))
+    (set-process-sentinel process 'handle-ipc-evt)
+    (setq discord-ipc-process process)
+    (send-json process discord-ipc-+handshake+ (ipc-handshake client-id))))
 
 (defun count-buffers ()
   (cl-count-if
@@ -75,6 +81,11 @@
          (not (string-match "^ " (buffer-name b)))))
    (buffer-list)))
 
+(defun get-current-major-mode ()
+  (let ((mode (assq 'major-mode (buffer-local-variables))))
+    (when mode
+      (cdr mode))))
+
 (defun start-time ()
   (let* ((uptime (string-to-number (emacs-uptime "%s")))
          (current-time (string-to-number (format-time-string "%s" (current-time)))))
@@ -82,14 +93,24 @@
 
 (defun gather-data ()
   (rich-presence :details (format "Editing buffer: %s" (buffer-name))
-                 :state (format "Buffers open: %d" (count-buffers))))
-                 ;;:startTimestamp (start-time)))
+                 :state (format "Buffers open: %d" (count-buffers))
+                 :timestamps `(:start ,(start-time))
+                 :assets `((large_image . ,(file-name-extension (buffer-name)))
+                           (large_text . ,(get-current-major-mode))
+                           (small_image . emacs)
+                           (small_text . emacs))))
 
-(defun discord-ipc-run (client-id &optional period) ;; TODO: update on emacs events
-  (let ((period (if period period (* 5 60))))
-    (when discord-ipc-client-timer
-      (cancel-timer discord-ipc-client-timer))
+(defun ipc-send-update ()
+  (unless (or (string= discord-ipc-current-buffer (buffer-name))
+              (string= (get-current-major-mode) "minbuffer-inactive-mode")) ; dont send messages when we enter the minibuffer
+    (setq discord-ipc-current-buffer (buffer-name))
+    (send-json discord-ipc-process discord-ipc-+frame+ (gather-data))))
+
+(defun discord-ipc-run (client-id)
+  (unless discord-ipc-started
     (setq discord-ipc-client-id client-id)
-    (setq discord-ipc-client-timer (run-at-time "1 sec" period 'ipc-update))))
+    (setq discord-ipc-started t)
+    (ipc-connect client-id)
+    (add-hook 'post-command-hook 'ipc-send-update)))
 
-(provide 'discord-ipc-run)
+(provide 'discord-ipc)
