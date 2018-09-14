446ee94174cc0f811d65ed3abdfe63a78f2b839d
Ben Simms <ben@bensimms.moe>
2018-09-14 21:07:56 +0100

    Discard unnused code, improve the blacklist regex

--- discord-emacs.el
+++ discord-emacs.el
@@ -1,11 +1,13 @@
 ;;; discord-emacs.el --- Discord ipc for emacs -*- lexical-binding: t -*-
 ;; Author: Ben Simms <ben@bensimms.moe>
-;; Version: 20180806
+;; Version: 20181014
 ;; URL: https://github.com/nitros12/discord-emacs.el
 
 (require 'json)
 (require 'bindat)
 (require 'cl-lib)
+
+;;; Code:
 
 (defvar discord-emacs--+handshake+ 0)
 (defvar discord-emacs--+frame+ 1)
@@ -24,10 +26,10 @@
 (defvar discord-emacs--current-buffer nil)
 (defvar discord-emacs--started nil)
 (defvar discord-emacs--blacklisted-buffer-names
-  '("^\\*Minibuf"
-    "^\\*NeoTree"))
+  '("^\\s *\\*"))
 
 (defun discord-emacs--maybe (x r)
+  "Shortcut for (if X X R)."
   (if x x r))
 
 (defun discord-emacs--get-ipc-url ()
@@ -39,13 +41,6 @@
   (make-network-process :name "discord-ipc-process"
                         :remote (discord-emacs--get-ipc-url)))
 
-(defun discord-emacs--handle-ipc-connection (process data)
-  "Handle an ipc connection on a PROCESS with DATA."
-  (let* ((decoded (discord-emacs--unpack-data data))
-         (evt (cdr (assq 'evt (cdr decoded)))))
-    (cond ((string= evt "READY")
-           (setq discord-emacs--current-connection process)))))
-
 (defun discord-emacs--pack-data (opcode data)
   "Pack OPCODE and DATA."
   (let ((encoded-json (json-encode data)))
@@ -53,22 +48,20 @@
                                        (length . ,(length encoded-json))
                                        (data . ,encoded-json)))))
 
-(defun discord-emacs--unpack-data (data)
-  (let ((unpacked (bindat-unpack discord-emacs--spec (string-as-unibyte data))))
-    (cons (bindat-get-field unpacked 'opcode)
-          (json-read-from-string (bindat-get-field unpacked 'data)))))
-
 (defun discord-emacs--ipc-handshake (client-id)
+  "Perform an ipc handshake with the client id CLIENT-ID."
   `((v .  ,discord-emacs--rich-presence-version)
     (client_id . ,client-id)))
 
 (defun discord-emacs--rich-presence (&rest fields)
+  "Build a rich presence payload with the fields FIELDS."
   `((cmd . "SET_ACTIVITY")
     (args . ((pid . ,(emacs-pid))
              (activity . ,fields)))
     (nonce . ,(number-to-string (random)))))
 
 (defun discord-emacs--send-json (opcode data)
+  "Send a JSON payload over the ipc connection with the opcode OPCODE and data DATA."
   (let ((process (get-process "discord-ipc-process")))
     (if (and process
              (process-live-p process))
@@ -76,12 +69,12 @@
       (discord-emacs--ipc-connect discord-emacs--client-id))))
 
 (defun discord-emacs--ipc-connect (client-id)
-  (let ((process (discord-emacs--make-ipc-connection)))
-    (set-process-filter process #'discord-emacs--handle-ipc-connection)
-    (discord-emacs--send-json discord-emacs--+handshake+ (discord-emacs--ipc-handshake client-id))
-    (setq discord-emacs--started t)))
+  "Make an ipc connection to discord with the client id CLIENT-ID."
+  (discord-emacs--send-json discord-emacs--+handshake+ (discord-emacs--ipc-handshake client-id))
+  (setq discord-emacs--started t))
 
 (defun discord-emacs--count-buffers ()
+  "Count the number of buffers."
   (cl-count-if
    (lambda (b)
      (or (buffer-file-name b)
@@ -89,16 +82,19 @@
    (buffer-list)))
 
 (defun discord-emacs--get-current-major-mode ()
+  "Get the current major mode of the active buffer."
   (let ((mode (assq 'major-mode (buffer-local-variables))))
     (when mode
       (symbol-name (cdr mode)))))
 
 (defun discord-emacs--start-time ()
+  "Get the start time of this Emacs instance."
   (let* ((uptime (string-to-number (emacs-uptime "%s")))
          (current-time (string-to-number (format-time-string "%s" (current-time)))))
     (- current-time uptime)))
 
 (defun discord-emacs--gather-data ()
+  "Gather data for a rich presence payload."
   (discord-emacs--rich-presence
    :details (format "Editing buffer: %s" (buffer-name))
    :state (format "Buffers open: %d" (discord-emacs--count-buffers))
@@ -109,10 +105,12 @@
              (small_text . "emacs"))))
 
 (defun discord-emacs--some-pred (predicates val)
+  "Apply all PREDICATES to VAL, return the first non-nil value or nil."
   (cl-some (lambda (pred) (funcall pred val))
            predicates))
 
 (defun discord-emacs--test-buffer ()
+  "Test if the current buffer is one that we should build a rich presence for."
   (discord-emacs--some-pred
    (cl-mapcar (lambda (regex)
                 (lambda (s) (string-match regex s)))
@@ -120,6 +118,7 @@
    (buffer-name)))
 
 (defun discord-emacs--ipc-send-update ()
+  "Send an ipc update to discord."
   (unless (or (string= discord-emacs--current-buffer (buffer-name))
               (discord-emacs--test-buffer))
     ;; dont send messages when we are in the same buffer or enter the minibuf
@@ -127,6 +126,7 @@
     (discord-emacs--send-json discord-emacs--+frame+ (discord-emacs--gather-data))))
 
 (defun discord-emacs-run (client-id)
+  "Run the rich presence with the client id CLIENT-ID."
   (unless discord-emacs--started
     (setq discord-emacs--client-id client-id)
     (add-hook 'post-command-hook #'discord-emacs--ipc-send-update)
